679a675351e0305bf23ac5785c7b8cc6e1e43cf3
luoquan <nchkdxlq.gmail.com>
2018-01-01 21:16:03 +0800

    init project

---
+++ SwiftTalk/SwiftTalk/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftTalk
+//
+//  Created by nchkdxlq on 2018/1/1.
+//  Copyright © 2018年 luoquan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
