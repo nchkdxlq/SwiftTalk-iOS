b2a36b3cd1d1dca6e66d902d9edc773682970de7
luoquan <nchkdxlq.gmail.com>
2018-01-07 14:37:37 +0800

    add: 添加目录分类

---
+++ SwiftTalk/SwiftTalk/Source/Client/SwiftTalk.swift
@@ -0,0 +1,27 @@
+//
+//  SwiftTalk.swift
+//  SwiftTalk
+//
+//  Created by nchkdxlq on 2018/1/7.
+//  Copyright © 2018年 luoquan. All rights reserved.
+//
+
+import Foundation
+
+public final class SwiftTalk: NSObject {
+    
+    static let share = SwiftTalk()
+    
+    private override init() {
+        
+    }
+    
+    public func userLogin() {
+        
+    }
+    
+    public func userRegister() {
+        
+    }
+    
+}
---
+++ SwiftTalk/SwiftTalk/Source/Login/RegisterViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RegisterViewController.swift
+//  SwiftTalk
+//
+//  Created by nchkdxlq on 2018/1/7.
+//  Copyright © 2018年 luoquan. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
